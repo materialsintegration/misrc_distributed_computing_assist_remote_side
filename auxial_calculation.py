8f5778f8b6187aeb3963289effcecf0c51f98b90
Yusuke Manaka <y-manaka@gitlab.mintsys.jp>
2022-07-27 16:37:44 +0900

    リリース

--- auxial_calculation.py
+++ auxial_calculation.py
@@ -20,6 +20,7 @@
 import os
 import sys
 import time
+import glob
 import shutil
 import datetime
 import subprocess
@@ -37,13 +38,12 @@ else:
 
 CALCDIR = None
 
-sys.exit(1)
 def error_log(messages):
     '''
     エラー/ワーニング出力→標準エラー出力
     '''
 
-    sys.stderr.write("%s:%s"%(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), messages))
+    sys.stderr.write("%s:%s\n"%(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), messages))
     sys.stderr.flush()
 
 def info_log(messages):
@@ -51,7 +51,7 @@ def info_log(messages):
     メッセージ出力→標準出力
     '''
 
-    sys.stdout.write("%s:%s"%(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), messages))
+    sys.stdout.write("%s:%s\n"%(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), messages))
     sys.stdout.flush()
 
 def preProcess():
@@ -67,7 +67,7 @@ def preProcess():
 
     info_log("事前処理を実行します。")
     #os.chdir("../")
-    cmd = 'ssh %s "cd ~/remote_calculation/%s; bash pre_process.sh'%(REMOTE_HOSTNAME, CALCDIR)
+    cmd = 'ssh %s "cd ~/remote_calculation/%s; bash pre_process.sh"'%(REMOTE_HOSTNAME, CALCDIR)
     print(cmd)
 
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -94,8 +94,8 @@ def postProcess():
     #    return False
 
     info_log("事後処理を実行します。")
-    os.chdir("../")
-    cmd = 'ssh %s "cd ~/remote_calculation/%s; bash post_process.sh'%(REMOTE_HOSTNAME, CALCDIR)
+    #os.chdir("../")
+    cmd = 'ssh %s "cd ~/remote_calculation/%s; bash post_process.sh"'%(REMOTE_HOSTNAME, CALCDIR)
     print(cmd)
 
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -199,14 +199,16 @@ def main():
     #preProcess関数でそれを読み込み、事前スクリプトとして実行する
     #※ qsub実行はできるが、終了待ち合わせはpre_process.shで実施すること。
     if os.path.exists("%s/resources/pre_process.sh"%BASE_DIR) is True:
-        shutil.copyfile("%s/resources/pre_process.sh"%BASE_DIR, "./pre_process.sh")
+        #shutil.copyfile("%s/resources/pre_process.sh"%BASE_DIR, "./pre_process.sh")
+        info_log("事前処理が予約されました。")
         isPreProcess = True
     # 事後処理
     #../resourcesディレクトリにpost_process.shファイルがあれば
     #postProcess関数でそれを読み込み、事前スクリプトとして実行する
     #※ qsub実行はできるが、終了待ち合わせはpre_process.shで実施すること。
     if os.path.exists("%s/resources/post_process.sh"%BASE_DIR) is True:
-        shutil.copyfile("%s/resources/post_process.sh"%BASE_DIR, "./post_process.sh")
+        #shutil.copyfile("%s/resources/post_process.sh"%BASE_DIR, "./post_process.sh")
+        info_log("事後処理が予約されました。")
         isPostProcess = True
     # qsubスクリプト
     if os.path.exists("%s/resources/qsub_process.sh"%BASE_DIR) is True:
@@ -214,6 +216,26 @@ def main():
     else:
         error_log("qsubプロセススクリプトファイルがありませんでした。")
         sys.exit(1)
+
+    # 事前処理または事後処理どちらかを実行する場合に、resources以下のファイルをコピーする（付属スクリプトなどのため）
+    if isPreProcess is True or isPostProcess is True:
+        #shutil.copyfile("%s/resources/*"%BASE_DIR, "./")
+        info_log("事前、事後処理用のファイルを準備します。")
+        cmd = "cp -rp %s/resources/* ./"%BASE_DIR
+        #copyfiles = glob.glob("%s/resources/*"%BASE_DIR)
+        #for item in copyfiles:
+        #    shutil.copy(item, "./")
+        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        proc.wait()
+        stdout, stderr = proc.communicate()
+        if proc.returncode != 0:
+
+            print("計算ディレクトリの準備に失敗しました")
+            print(stdout.decode("UTF-8"))
+            print(stderr.decode("UTF-8"))
+    
+            sys.exit(1)
 
     # データを転送
     info_log("データを転送します。")
@@ -235,6 +257,7 @@ def main():
 
     # 事前処理
     if isPreProcess is True:
+        info_log("事前処理を実行します。")
         ret = preProcess()
         if ret is False:
             error_log("事前処理が異常終了しました。")
@@ -247,6 +270,7 @@ def main():
 
     # 事後処理
     if isPostProcess is True:
+        info_log("事後処理を実行します。")
         ret = postProcess()
         if ret is False:
             error_log("事後処理が異常終了しました。")
@@ -254,6 +278,7 @@ def main():
 
     # 計算結果の取り込み
     info_log("計算ディレクトリ内容を転送します。")
+    os.chdir(current_dir)                         # ~/remote_calculation/計算ディレクトリへ移動
     cmd = "scp -rp nims-spacom:~/remote_calculation/%s/* ./"%CALCDIR
     info_log(cmd)
 
@@ -274,106 +299,4 @@ if __name__ == "__main__":
 
     #execute_fister_dpf()
     main()
-
-def execute_fister_dpf(nprocs="48", path_hazmesh_ccv="hazmeshccv.in"):
-    """
-    FrontISTRをNIMS SuperComputer環境下で実行する
-    @param nprocs(string) ノードまたいだ並列処理数（stringで指定）
-    @param path_hazmesh_ccv(string)
-    """
-
-    # ログの設定
-    logger = setup_logger()
-
-    # 準備
-    logger.info("ワークフロー実行開始")
-
-    ## パスの定義
-    path_inp = "hazmeshccv.inp"
-    path_calc_cond = "calccond.txt"
-    path_matpara = "matpara.xml"
-    path_fister_result = 'analyze.dat'
-
-    # inpファイルの変換
-    logger.info("inpファイルを変換")
-
-    ## property.xmlから試験条件取得
-    project_property = prop2dict("property.xml")
-
-    ## matpara.xmlから材料定数取得 現状は材料名決め打ち
-    material_property = mat2dict("matpara.xml")
-    base_metal = [material for material in material_property if material["name"] == "MaW_BM"][0]
-    haz_metal = [material for material in material_property if material["name"] == "MaW_HAZ"][0]
-
-    ## inpつくる
-    in2inp.convert(
-        input_file=path_hazmesh_ccv,
-        output_file=path_inp,
-        load=project_property["Pressure"],
-        bme=base_metal["elastic"]["youngs_modulus"] * 10 ** 3, # [GPa] -> [MPa]
-        bmn=base_metal["elastic"]["poisson_ratio"],
-        haze=haz_metal["elastic"]["youngs_modulus"] * 10 ** 3, # [GPa] -> [MPa]
-        hazn=haz_metal["elastic"]["poisson_ratio"]
-    )
-
-    # hamの実行
-    logger.info("hamを実行")
-    ham(inp=path_inp, calccond=path_calc_cond, matpara=path_matpara)
-
-    ## hecmw_ctrlの出力
-    logger.info("hecmw_ctrlを出力中")
-    hecmw_ctrl()
-
-    ## hecmw_partの出力
-    logger.info("hecmw_partを出力中")
-    hecmw_part(nprocs)
-
-    ## パーティション
-    logger.info("パーティショナーを実行します")
-    subprocess.run("hecmw_part1", shell=True)
-
-    # FrontISTRの実行
-    logger.info("FrontISTRを実行します。")
-
-    current_dir = os.getcwd()
-
-    # qsubスクリプト生成
-    outfile = open("qsub_fistr.sh", "w")
-    outfile.write("#!/bin/bash\n")
-    outfile.write("#QSUB2 queue qM004\n")
-    outfile.write("#QSUB2 core 48\n")
-    outfile.write("#QSUB2 mpi 1\n")
-    outfile.write("#QSUB2 smp 1\n")
-    outfile.write("#QSUB2 wtime 48:00:00\n")
-    outfile.write("#PBS -N frontISTRx48x1\n")
-    outfile.write("\n")
-    outfile.write(". ~/local/intel/oneapi/setvars.sh --force\n")
-    #outfile.write("cd ~/remote-calculation/%s\n"%os.path.basename(current_dir))
-    outfile.write("cd $PBS_O_WORKDIR\n")
-    #outfile.write('echo "PBS_NODEFILE = "$PBS_NODEFILE\n')
-    #outfile.write("NUMPROCS=`wc -l < $PBS_NODEFILE`\n")
-    #outfile.write('echo "NUMPROCS = "$NUMPROCS\n')
-    outfile.write("time ~/local/intel/oneapi/mpi/latest/bin/mpirun -np 48 fistr1 -t 1 > fistr_qsub.log 2>&1\n")
-    outfile.write("\n")
-    outfile.close()
-
-    # 計算結果を抽出
-    info_log("計算結果の抽出。")
-    with open(path_fister_result, "r") as f:
-
-        fister_result = f.read()
-    
-    huddlestone_str = [line for line in fister_result.split("\n") if "huddleston stress" in line][0]
-    huddlestone_stress_pre = re.search("[0-9E\.\+]+", huddlestone_str)
-
-    if huddlestone_stress_pre is None:
-
-        error_log("huddleston stressはNoneです。")
-        sys.exit(0)
-
-    huddlestone_stress = float(huddlestone_stress_pre.group())
-
-    with open("huddleston.dat", "w") as f:
-
-        f.write(str(huddlestone_stress))
 
